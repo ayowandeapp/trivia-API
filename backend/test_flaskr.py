9d21313c76feed9fe4d731e88ac44a5b65833ef1
ayowande <66342315+ayowandeapp@users.noreply.github.com>
2022-08-17 10:35:52 +0100

    Add files via upload

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,12 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        self.DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
+        self.DB_USER = os.getenv('DB_USER', 'postgres')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD', 'admin')
+        self.DB_NAME = os.getenv('DB_NAME', 'trivia_test')
+        self.database_path = 'postgresql+psycopg2://{}:{}@{}/{}'.format(self.DB_USER, self.DB_PASSWORD, self.DB_HOST, self.DB_NAME)
+        #self.database_path = "postgresql://{}/{}".format(self.DB_HOST, self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -111,7 +116,8 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data['questions']))
 
     def test_get_question_search_with_results(self):
-        res = self.client().post('/questions/search',json=('searchTerm':'which'))
+        search= {'searchTerm':'which'}
+        res = self.client().post('/questions/search',json=search)
         data = json.loads(res.data)
         self.assertEqual(res.status_code,200)
         self.assertEqual(data['success'],True)
@@ -119,7 +125,8 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['questions'],9)
 
     def test_get_question_search_without_results(self):
-        res = self.client().post('/questions/search',json=('searchTerm':''))
+        search= {'searchTerm':''}
+        res = self.client().post('/questions/search',json=search)
         data = json.loads(res.data)
         self.assertEqual(res.status_code,404)
         self.assertEqual(data['message'],False)
